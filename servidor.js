acdcf6d357aecc50880bf07f1f03e34b86390ea6
ysaiascampos <ysaiascampos@gmail.com>
2020-09-26 18:42:55 -0300

    final

--- servidor.js
+++ servidor.js
@@ -7,9 +7,18 @@ const socketio = require('socket.io');
 const io = socketio(servidor);
 
 io.on('connection', socket => {
-    socket.on('conectado', () => {
-        console.log("Usuario conectado")
-    })
+    let nombre;
+    socket.on('conectado', (nomb) => {
+        nombre=nomb;
+        socket.broadcast.emit("mensajes", {nombre: nombre, mensaje:`${nombre} ha entrado a la sala del chat`});
+    });
+    socket.on('mensaje', (nombre, mensaje) => {
+        io.emit("mensajes", {nombre, mensaje});
+    });
+    socket.on('disconnect', () => {
+        io.emit("mensajes", {servidor:"Servidor", mensaje:`${nombre} Ha abandonado la sala`});
+    });
+
 })
 
 servidor.listen(3000, () => console.log("Servidor inicializar"));
